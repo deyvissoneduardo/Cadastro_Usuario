1bda8c520540cf4e57cfea827e9a1b1ca6e53e5e
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-06-08 23:16:49 -0300

    class user

---
+++ cadastro_usuario/lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:cadastrousuario/model/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Deyvisson Eduardo',
+      email: 'deyvisson@email.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Rayanne Silva',
+      email: 'ray@email.com',
+      avatarUrl:
+      'https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_960_720.png'),
+  '3': const User(
+      id: '2',
+      name: 'Maraia Silva',
+      email: 'maria@email.com',
+      avatarUrl:
+      'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'),
+};
