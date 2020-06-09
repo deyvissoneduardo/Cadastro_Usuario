dd5c813572119a7e90a55bfe5bab35388e09e826
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-06-09 14:52:24 -0300

    method salve user

--- cadastro_usuario/lib/provider/users.dart
+++ cadastro_usuario/lib/provider/users.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cadastrousuario/data/dummy_users.dart';
 import 'package:cadastrousuario/model/user.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +15,36 @@ class Users with ChangeNotifier {
     return _items.length;
   }
 
-  User byItem(int index){
+  User byItem(int index) {
     return _items.values.elementAt(index);
+  }
+
+  void put(User user) {
+    if (user == null) {
+      return;
+    }
+    if (user.id != null &&
+        user.id.trim().isNotEmpty &&
+        _items.containsKey(user.id)) {
+      _items.update(
+        user.id,
+        (_) => User(
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            avatarUrl: user.avatarUrl),
+      );
+    } else {
+      final id = Random().nextDouble().toString();
+      _items.putIfAbsent(
+        id,
+        () => User(
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            avatarUrl: user.avatarUrl),
+      );
+    }
+    notifyListeners();
   }
 }
